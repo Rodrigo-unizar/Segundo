8a1905f37cf4fb107a3171c8b1197ca5eb721b23
Rodrigo-unizar <935413@unizar.es>
2025-12-03 15:30:26 +0100

    Trabajo de Redes

--- Primer_Cuatri/Redes/Practicas/P4/rcftpclient/misfunciones.c
+++ Primer_Cuatri/Redes/Practicas/P4/rcftpclient/misfunciones.c
@@ -23,6 +23,8 @@
 #include <time.h>
 #include <sys/time.h>
 #include <math.h>
+#include <signal.h>
+#include <stdbool.h>
 #include "rcftp.h" // Protocolo RCFTP
 #include "rcftpclient.h" // Funciones ya implementadas
 #include "multialarm.h" // Gestión de timeouts
@@ -32,10 +34,10 @@
 /**************************************************************************/
 /* VARIABLES GLOBALES                                                     */
 /**************************************************************************/
-#warning HAY QUE PONER EL NOMBRE (Y BORRAR EL WARNING)
+
 // elegir 1 o 2 autores y sustituir "Apellidos, Nombre" manteniendo el formato
-char* autores="Autor: Apellidos, Nombre"; // un solo autor
-//char* autores="Autor: Apellidos, Nombre\nAutor: Apellidos, Nombre" // dos autores
+//char* autores="Autor: Apellidos, Nombre"; // un solo autor
+char* autores="Autor: Ghita Calangiu, Razvan\nAutor: Herce Alonso, Rodrigo";	 // dos autores
 
 // variable para indicar si mostrar información extra durante la ejecución
 // como la mayoría de las funciones necesitaran consultarla, la definimos global
@@ -50,20 +52,283 @@ extern volatile const int timeouts_vencidos;
 /**************************************************************************/
 /* Obtiene la estructura de direcciones del servidor */
 /**************************************************************************/
-struct addrinfo* obtener_struct_direccion(char *dir_servidor, char *servicio, char f_verbose){
-#warning COPIA AQUI LA FUNCION CORRESPONDIENTE DE COMUN.C (PRACTICA 3)
+struct addrinfo* obtener_struct_direccion(char *dir_servidor, char *servicio, char f_verbose)
+{
+    struct addrinfo hints,     // Variable para especificar la solicitud
+                    *servinfo, // Puntero para respuesta de getaddrinfo()
+                    *direccion;// Puntero para recorrer la lista de
+                               // direcciones de servinfo
+    int status;     // Finalización correcta o no de la llamada getaddrinfo()
+    int numdir = 1; // Contador de estructuras de direcciones en la
+                    // lista de direcciones de servinfo
+
+    // sobreescribimos con ceros la estructura
+    // para borrar cualquier dato que pueda malinterpretarse
+    memset(&hints, 0, sizeof hints);
+
+    // genera una estructura de dirección con especificaciones de la solicitud
+    if (f_verbose)
+    {
+        printf("1 - Especificando detalles de la estructura de direcciones a solicitar... \n");
+        fflush(stdout);
+    }
+
+    // especificamos la familia de direcciones con la que queremos trabajar:
+    // AF_UNSPEC, AF_INET (IPv4), AF_INET6 (IPv6), etc.
+    hints.ai_family = AF_UNSPEC;
+
+    if (f_verbose)
+    {
+        printf("\tFamilia de direcciones/protocolos: ");
+        switch (hints.ai_family)
+        {
+            case AF_UNSPEC: printf("IPv4 e IPv6\n"); break;
+            case AF_INET:   printf("IPv4\n"); break;
+            case AF_INET6:  printf("IPv6\n"); break;
+            default:        printf("No IP (%d)\n", hints.ai_family); break;
+        }
+        fflush(stdout);
+    }
+
+    // especificamos el tipo de socket deseado:
+    // SOCK_STREAM (TCP), SOCK_DGRAM (UDP), etc.
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if (f_verbose)
+    {
+        printf("\tTipo de comunicación: ");
+        switch (hints.ai_socktype)
+        {
+            case SOCK_STREAM: printf("flujo (TCP)\n"); break;
+            case SOCK_DGRAM:  printf("datagrama (UDP)\n"); break;
+            default:          printf("no convencional (%d)\n", hints.ai_socktype); break;
+        }
+        fflush(stdout);
+    }
+
+    // flags específicos dependiendo de si queremos la dirección como cliente
+    // o como servidor
+    if (dir_servidor != NULL)
+    {
+        // si hemos especificado dir_servidor, es que somos el cliente
+        // y vamos a conectarnos con dir_servidor
+        if (f_verbose) printf("\tNombre/dirección del equipo: %s\n", dir_servidor);
+    }
+    else
+    {
+        // si no hemos especificado, es que vamos a ser el servidor
+        if (f_verbose) printf("\tNombre/dirección: equipo local\n");
+
+        // especificar flag para que la IP se rellene con lo necesario para hacer bind
+        // consultar documentación con: 'man getaddrinfo')
+        hints.ai_flags = AI_PASSIVE;
+    }
+    if (f_verbose) printf("\tServicio/puerto: %s\n", servicio);
+
+    // llamada getaddrinfo() para obtener la estructura de direcciones solicitada
+    // getaddrinfo() pide memoria dinámica al SO,
+    // la rellena con la estructura de direcciones
+    // y escribe en servinfo la dirección donde se encuentra dicha estructura.
+    // La memoria dinámica reservada en una función NO se libera al salir de ella
+    // Para liberar esta memoria, usar freeaddrinfo()
+    if (f_verbose)
+    {
+        printf("2 - Solicitando la estructura de direcciones con getaddrinfo()... ");
+        fflush(stdout);
+    }
+    status = getaddrinfo(dir_servidor, servicio, &hints, &servinfo);
+    if (status != 0)
+    {
+        fprintf(stderr,"Error en la llamada getaddrinfo(): %s\n", gai_strerror(status));
+        exit(1);
+    }
+    if (f_verbose) printf("hecho\n");
+
+    // imprime la estructura de direcciones devuelta por getaddrinfo()
+    if (f_verbose)
+    {
+        printf("3 - Analizando estructura de direcciones devuelta... \n");
+        direccion = servinfo;
+        while (direccion != NULL)
+        {   // bucle que recorre la lista de direcciones
+            printf("    Dirección %d:\n", numdir);
+            printsockaddr((struct sockaddr_storage*) direccion->ai_addr);
+            // "avanzamos" a la siguiente estructura de direccion
+            direccion = direccion->ai_next;
+            numdir++;
+        }
+    }
+
+    // devuelve la estructura de direcciones devuelta por getaddrinfo()
+    return servinfo;
 }
 /**************************************************************************/
 /* Imprime una direccion */
 /**************************************************************************/
-void printsockaddr(struct sockaddr_storage * saddr) {
-#warning COPIA AQUI LA FUNCION CORRESPONDIENTE DE COMUN.C PARA UDP (PRACTICA 3)
+void printsockaddr(struct sockaddr_storage * saddr)
+{
+    struct sockaddr_in *saddr_ipv4; // puntero a estructura de dirección IPv4
+    // el compilador interpretará lo apuntado como estructura de dirección IPv4
+    struct sockaddr_in6 *saddr_ipv6; // puntero a estructura de dirección IPv6
+    // el compilador interpretará lo apuntado como estructura de dirección IPv6
+    void *addr; // puntero a dirección
+    // como puede ser tipo IPv4 o IPv6 no queremos que el compilador la
+    // interprete de alguna forma particular, por eso void
+    char ipstr[INET6_ADDRSTRLEN]; // string para la dirección en formato texto
+    int port; // almacena el número de puerto al analizar estructura devuelta
+
+    if (saddr == NULL)
+    {
+        printf("La dirección está vacía\n");
+    }
+    else
+    {
+        printf("\tFamilia de direcciones: ");
+        fflush(stdout);
+        if (saddr->ss_family == AF_INET6)
+        {   // IPv6
+            printf("IPv6\n");
+            // apuntamos a la estructura con saddr_ipv6 (cast evita warning),
+            // así podemos acceder al resto de campos a través de
+            // este puntero sin más casts
+            saddr_ipv6 = (struct sockaddr_in6 *)saddr;
+            // apuntamos al campo de la estructura que contiene la dirección
+            addr = &(saddr_ipv6->sin6_addr);
+            // obtenemos el puerto, pasando del formato de red al formato local
+            port = ntohs(saddr_ipv6->sin6_port);
+        }
+        else if (saddr->ss_family == AF_INET)
+        {   // IPv4
+            printf("IPv4\n");
+            saddr_ipv4 = (struct sockaddr_in*)saddr ;
+            addr = &(saddr_ipv4->sin_addr);
+            port = ntohs(saddr_ipv4->sin_port);
+        }
+        else
+        {
+            fprintf(stderr, "familia desconocida\n");
+            exit(1);
+        }
+        // convierte la dirección ip a string
+        inet_ntop(saddr->ss_family, addr, ipstr, sizeof ipstr);
+        printf("\tDirección (interpretada según familia): %s\n", ipstr);
+        printf("\tPuerto (formato local): %d\n", port);
+    }
 }
 /**************************************************************************/
 /* Configura el socket, devuelve el socket y servinfo */
 /**************************************************************************/
 int initsocket(struct addrinfo *servinfo, char f_verbose){
-#warning COPIA AQUI LA FUNCION CORRESPONDIENTE DEL CLIENTEVOCALESUDP.C (PRACTICA 3)
+    int sock = -1;
+
+    // crea un extremo de la comunicación y devuelve un descriptor
+    if(f_verbose){
+    	printf("Creando el socket (socket)... ");
+        fflush(stdout);
+    }
+
+    sock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if(sock < 0){
+    	perror("Error en la llamada socket: No se pudo crear el socket");
+        // muestra por pantalla el valor de la cadena suministrada por el
+        // programador, dos puntos y un mensaje de error que detalla la
+        // causa del error cometido
+
+    } else  {   // socket creado correctamente
+        if (f_verbose) printf("hecho\n");
+    }
+
+    if (sock < 0)
+    {
+        perror("No se ha podido establecer la comunicación");
+        exit(1);
+    }
+
+    return sock;
+}
+
+
+void construirPMensajeRCFTP(struct rcftp_msg *sendbuffer, int datos) {
+    // rellenar sendbuffer proporcionado
+    sendbuffer->version = RCFTP_VERSION_1;
+    sendbuffer->numseq = htonl(0);
+    sendbuffer->len = htons(datos);
+    sendbuffer->next = htonl(0);
+    sendbuffer->flags = 0;
+    sendbuffer->sum = 0;
+    // calcular checksum sobre toda la estructura
+    sendbuffer->sum = xsum((char*)sendbuffer, sizeof(*sendbuffer));
+}
+
+
+void enviamensaje(int s, struct rcftp_msg sendbuffer, struct sockaddr_storage remote, socklen_t remotelen, unsigned int flags) {
+	ssize_t sentsize;
+
+	if ((sentsize=sendto(s,(char *)&sendbuffer,sizeof(sendbuffer),0,(struct sockaddr *)&remote,remotelen)) != sizeof(sendbuffer)) {
+		if (sentsize!=-1)
+			fprintf(stderr,"Error: enviados %d bytes de un mensaje de %d bytes\n",(int)sentsize,(int)sizeof(sendbuffer));
+		else
+			perror("Error en sendto");
+		exit(S_SYSERROR);
+	} 
+
+	// print response if in verbose mode
+	if (flags & F_VERBOSE) {
+		printf("Mensaje RCFTP " ANSI_COLOR_MAGENTA "enviado" ANSI_COLOR_RESET ":\n");
+		print_rcftp_msg(&sendbuffer,sizeof(sendbuffer));
+	} 
+}	
+
+
+ssize_t recibirmensaje(int socket, struct rcftp_msg *buffer, int buflen, struct sockaddr_storage *remote, socklen_t *remotelen) {
+	ssize_t recvsize;
+	
+	*remotelen = sizeof(*remote);
+	recvsize = recvfrom(socket,(char *)buffer,buflen,0,(struct sockaddr *)remote,remotelen);
+	if (recvsize<0 && errno!=EAGAIN) { // en caso de socket no bloqueante
+		//if (recvsize<0 && errno!=EINTR) { // en caso de socket bloqueante (no funciona en GNU/Linux)
+		perror("Error en recvfrom: ");
+		exit(S_SYSERROR);
+	} else if (*remotelen>sizeof(*remote)) {
+		fprintf(stderr,"Error: la dirección del cliente ha sido truncada\n");
+		exit(S_SYSERROR);
+	}
+
+	return recvsize;
+}
+
+int esMensajeValido(struct rcftp_msg msgRecibido) {
+    int esperado=1;
+    //uint16_t aux;
+
+    if (msgRecibido.version!=RCFTP_VERSION_1) { // versión incorrecta
+        esperado=0;
+        fprintf(stderr,"Error: recibido un mensaje con versión incorrecta\n");
+    }
+    /*if (msgRecibido.next!=0) { // next incorrecto
+        esperado=0;
+        fprintf(stderr,"Error: recibido un mensaje con NEXT incorrecto\n");
+    }*/
+    if (issumvalid(&msgRecibido,sizeof(msgRecibido))==0) { // checksum incorrecto
+        esperado=0;
+        fprintf(stderr,"Error: recibido un mensaje con checksum incorrecto\n"); /* (esperaba ");
+        aux=recvbuffer.sum;
+        recvbuffer.sum=0;
+        fprintf(stderr,"0x%x)\n",ntohs(xsum((char*)&recvbuffer,sizeof(recvbuffer))));
+        recvbuffer.sum=aux;*/
+    }
+    return esperado;
+}
+
+int esMensajeEsperado(struct rcftp_msg msgRecibido, struct rcftp_msg mensajeEsperado){
+    if(ntohl(msgRecibido.next) != (ntohl(mensajeEsperado.numseq) + ntohs(mensajeEsperado.len))){
+        return 0;
+    }else if(msgRecibido.flags & F_BUSY || msgRecibido.flags & F_ABORT){
+        return 0;
+    }else if(mensajeEsperado.flags & F_FIN && !(msgRecibido.flags & F_FIN)){
+        return 0;
+    }
+    return 1;
 }
 
 
@@ -74,19 +339,171 @@ void alg_basico(int socket, struct addrinfo *servinfo) {
 
 	printf("Comunicación con algoritmo básico\n");
 
-#warning FALTA IMPLEMENTAR EL ALGORITMO BASICO
-	printf("Algoritmo no implementado\n");
+	bool ultimoMensaje = false;
+	bool ultimoMensajeConfirmado = false;
+	struct rcftp_msg mensaje;
+    struct rcftp_msg msgRecibido;
+	struct sockaddr_storage remote;
+	unsigned int flags = F_VERBOSE;
+    socklen_t remotelen;
+
+    memcpy(&remote, servinfo->ai_addr, servinfo->ai_addrlen);
+    remotelen = servinfo->ai_addrlen;
+
+	int datos = readtobuffer((char *)mensaje.buffer, RCFTP_BUFLEN);
+
+	if(datos == 0) {
+		ultimoMensaje = true;
+	}
+	
+	construirPMensajeRCFTP(&mensaje, datos);
+
+	while(!ultimoMensajeConfirmado){
+		enviamensaje(socket, mensaje,remote,remotelen,flags);
+        if(recibirmensaje(socket, &msgRecibido, sizeof(msgRecibido), &remote, &remotelen) > 0){
+            if(esMensajeValido(msgRecibido) && esMensajeEsperado(msgRecibido, mensaje)){
+                if(ultimoMensaje){
+                    ultimoMensajeConfirmado = true;
+                } else {
+                    datos = readtobuffer((char *)mensaje.buffer, RCFTP_BUFLEN);
+
+                    if(datos <= 0) {
+                        ultimoMensaje = true;
+                        mensaje.flags = F_FIN;
+                    }
+
+                if(!ultimoMensaje){
+                    mensaje.flags = 0;  //no tiene flags
+                }
+                    //Construimos el siguiente mensaje
+                    mensaje.version = RCFTP_VERSION_1;
+                    mensaje.numseq = msgRecibido.next;
+                    mensaje.len = htons(datos);
+                    mensaje.next = htonl(0);
+                    mensaje.sum = 0;
+                    // calcular checksum sobre toda la estructura
+                    mensaje.sum = xsum((char*)&mensaje, sizeof(mensaje));
+            
+                }
+            }
+	    }
+    }
 }
+
+ssize_t recibirmensajeSW(int socket, struct rcftp_msg *buffer, int buflen, struct sockaddr_storage *remote, socklen_t *remotelen, int timeouts_procesados) {
+	
+    bool esperar =true;
+    ssize_t recvsize;
+    *remotelen = sizeof(*remote);
+
+     
+
+    while(esperar){
+      
+        recvsize = recvfrom(socket,(char *)buffer,buflen,0,(struct sockaddr *)remote,remotelen);
+        if (recvsize<0 && errno!=EAGAIN) { // en caso de socket no bloqueante
+            //if (recvsize<0 && errno!=EINTR) { // en caso de socket bloqueante (no funciona en GNU/Linux)
+            perror("Error en recvfrom: ");
+            exit(S_SYSERROR);
+        } else if (*remotelen>sizeof(*remote)) {
+            fprintf(stderr,"Error: la dirección del cliente ha sido truncada\n");
+            exit(S_SYSERROR);
+        }else if(recvsize>0){
+            canceltimeout();
+            esperar=false;
+        }
+        if (timeouts_procesados < timeouts_vencidos){
+            esperar = false;
+            timeouts_procesados++;
+        }
+        
+    }
+    
+	return recvsize;
+}
+
+void enviamensajeSW(int s, struct rcftp_msg sendbuffer, struct sockaddr_storage remote, socklen_t remotelen, unsigned int flags) {
+	ssize_t sentsize;
+
+	if ((sentsize=sendto(s,(char *)&sendbuffer,sizeof(sendbuffer),0,(struct sockaddr *)&remote,remotelen)) != sizeof(sendbuffer)) {
+		if (sentsize!=-1)
+			fprintf(stderr,"Error: enviados %d bytes de un mensaje de %d bytes\n",(int)sentsize,(int)sizeof(sendbuffer));
+		else
+			perror("Error en sendto");
+		exit(S_SYSERROR);
+	} 
+
+	// print response if in verbose mode
+	if (flags & F_VERBOSE) {
+		printf("Mensaje RCFTP " ANSI_COLOR_MAGENTA "enviado" ANSI_COLOR_RESET ":\n");
+		print_rcftp_msg(&sendbuffer,sizeof(sendbuffer));
+	} 
+    //alarm(duracion_timeout); // iniciar temporizador
+}	
 
 /**************************************************************************/
 /*  algoritmo 2 (stop & wait)  */
 /**************************************************************************/
+
 void alg_stopwait(int socket, struct addrinfo *servinfo) {
 
 	printf("Comunicación con algoritmo stop&wait\n");
 
-#warning FALTA IMPLEMENTAR EL ALGORITMO STOP-WAIT
-	printf("Algoritmo no implementado\n");
+    signal(SIGALRM, handle_sigalrm); // definir manejador de la señal de alarma
+    int sockflags = fcntl(socket, F_GETFL, 0); //obtiene valor de los flags 
+    fcntl(socket, F_SETFL, sockflags | O_NONBLOCK); //modifica el flag de bloqueo
+    int timeouts_procesados = 0;
+
+    bool ultimoMensaje = false;
+	bool ultimoMensajeConfirmado = false;
+	struct rcftp_msg mensaje;
+    struct rcftp_msg msgRecibido;
+	struct sockaddr_storage remote;
+	unsigned int flags = F_VERBOSE;
+    socklen_t remotelen;
+
+    memcpy(&remote, servinfo->ai_addr, servinfo->ai_addrlen);
+    remotelen = servinfo->ai_addrlen;
+
+	int datos = readtobuffer((char *)mensaje.buffer, RCFTP_BUFLEN);
+
+	if(datos == 0) {
+		ultimoMensaje = true;
+	}
+	
+	construirPMensajeRCFTP(&mensaje, datos);
+
+	while(!ultimoMensajeConfirmado){
+		enviamensaje(socket, mensaje,remote,remotelen,flags);
+        addtimeout();
+        if(recibirmensajeSW(socket, &msgRecibido, sizeof(msgRecibido), &remote, &remotelen, timeouts_procesados) > 0){
+            if(esMensajeValido(msgRecibido) && esMensajeEsperado(msgRecibido, mensaje)){
+                if(ultimoMensaje){
+                    ultimoMensajeConfirmado = true;
+                } else {
+                    datos = readtobuffer((char *)mensaje.buffer, RCFTP_BUFLEN);
+
+                    if(datos <= 0) {
+                        ultimoMensaje = true;
+                        mensaje.flags = F_FIN;
+                    }
+
+                if(!ultimoMensaje){
+                    mensaje.flags = 0;  //no tiene flags
+                }
+                    //Construimos el siguiente mensaje
+                    mensaje.version = RCFTP_VERSION_1;
+                    mensaje.numseq = msgRecibido.next;
+                    mensaje.len = htons(datos);
+                    mensaje.next = htonl(0);
+                    mensaje.sum = 0;
+                    // calcular checksum sobre toda la estructura
+                    mensaje.sum = xsum((char*)&mensaje, sizeof(mensaje));
+            
+                }
+            }
+	    }
+    }
 }
 
 /**************************************************************************/
--- Primer_Cuatri/Redes/Practicas/P4/rcftpclient/misfunciones.h
+++ Primer_Cuatri/Redes/Practicas/P4/rcftpclient/misfunciones.h
@@ -14,6 +14,30 @@
  * @param[in] f_verbose Flag para imprimir información adicional
  * @return Dirección de estructura con la dirección del servidor
  */
+ /* Flags del programa */
+/** @{ */
+#define F_VERBOSE	0x1	/**< Flag para mostrar detalles en la salida estándar */
+// a bailar!
+#define F_SALSA		0x2	/**< Flag para generar respuestas incorrectas y descartando mensajes recibidos */
+#define F_FUNKY		0x4	/**< F_SALSA + puede no responder o duplicar mensaje */
+#define F_ROCKNROLL	0x8 /**< F_FUNKY + cualquier error, con/sin descartar mensajes recibidos */
+/** @} */
+/* defines para la salida del programa */
+/** @{ */
+#define S_OK 0 /**< Flag de salida correcta */
+#define S_ABORT 1 /**< Flag de salida incorrecta */
+#define S_SYSERROR 2 /**< Flag de salida por error de sistema */
+#define S_PROGERROR 3 /**< Flag de salida por error de programa (BUG!) */
+#define S_CLIERROR 4 /**< Flag de salida para avisar de error en cliente */
+/** @} */
+/** @{ */
+#define ANSI_COLOR_RED     "\x1b[31m" /**< Pone terminal a rojo */
+#define ANSI_COLOR_GREEN   "\x1b[32m" /**< Pone terminal a verde */
+#define ANSI_COLOR_YELLOW  "\x1b[33m" /**< Pone terminal a amarillo */
+#define ANSI_COLOR_BLUE    "\x1b[34m" /**< Pone terminal a azul */
+#define ANSI_COLOR_MAGENTA "\x1b[35m" /**< Pone terminal a magenta */
+#define ANSI_COLOR_CYAN    "\x1b[36m" /**< Pone terminal a turquesa */
+#define ANSI_COLOR_RESET   "\x1b[0m" /**< Desactiva color del terminal */
 struct addrinfo* obtener_struct_direccion(char *dir_servidor, char *servicio, char f_verbose);
 
 /**
